dbe7f2e1b5e82b0cc244444ea021ebd417930da4
dogaska <ndogakaracik@gmail.com>
2024-06-29 23:21:40 +0200

    updates navigation bar

--- frontend/src/components/homepage/hero-section.tsx
+++ frontend/src/components/homepage/hero-section.tsx
@@ -3,11 +3,11 @@ export function HeroSection() {
     <div className=" bg-white h-full">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
-          className="fixed transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="fixed transform-gpu overflow-hidden blur-3xl sm:-top-85"
           aria-hidden="false"
         >
           <div
-            className=" relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#DCE546] to-[#46E59F] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="z-0 relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 -translate-y-1/2 rotate-[30deg] bg-gradient-to-tr from-[#46E59F] to-[#DCE546] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -52,7 +52,7 @@ export function HeroSection() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#DCE546] to-[#46E59F] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="z-0 relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#DCE546] to-[#46E59F] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
