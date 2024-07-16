3e2af6268ef40968a501be8f37cf9ce09e227c29
dogaska <ndogakaracik@gmail.com>
2024-07-16 16:28:06 +0200

    make changes for deployment

--- backend/sonnennetz/settings.py
+++ backend/sonnennetz/settings.py
@@ -30,9 +30,9 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -97,7 +97,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "sonnennetz.wsgi.application"
+WSGI_APPLICATION = "sonnennetz.wsgi.app"
 
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
