5c5cf69400f8d10bdcea2d43a9f776bc89faeeaf
erayyildiztum <yildizzeray17@gmail.com>
2024-07-17 12:35:25 +0200

    enables debugging

--- backend/sonnennetz/deployment.py
+++ backend/sonnennetz/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 print(ALLOWED_HOSTS)
 CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]]
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 MIDDLEWARE = [
