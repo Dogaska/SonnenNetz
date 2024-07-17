98b8ec377ff315d23db2b52064d92d6e689d3de4
erayyildiztum <yildizzeray17@gmail.com>
2024-07-17 05:13:40 +0200

    update

--- backend/sonnennetz/settings.py
+++ backend/sonnennetz/settings.py
@@ -27,7 +27,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["MY_SECRET_KEY"]
+SECRET_KEY = 'django-insecure-0rzj4a7f_#u(gwc^*n&ygyg)1vx62lkd!zy5g#1wk6)_+0s39s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
